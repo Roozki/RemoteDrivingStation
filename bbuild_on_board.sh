331652c28f34305a648edfde91d7d66c26b9c29b
Roozki <zawadzkirowan@gmail.com>
2024-03-30 10:09:59 -0700

    bbuildonboard

--- bbuild_on_board.sh
+++ bbuild_on_board.sh
@@ -1,3 +1,3 @@
-rm -rf build/ log/ install/
-colcon build --packages-ignore rds_hud rds_sims rds_control rds_gui
+#rm -rf build/ log/ install/
+colcon build --symlink-install --packages-ignore rds_hud rds_sims rds_control rds_gui
 source install/setup.bash
