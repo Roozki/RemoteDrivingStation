0115dcb0611262747c2d0a6dd84342fb3deacc0d
Rowan Zawadzki <zawadzkirowan@gmail.com>
2024-03-31 16:40:34 -0700

    reshn

--- bbuild_on_board.sh
+++ bbuild_on_board.sh
@@ -1,3 +1,5 @@
 #rm -rf build/ log/ install/
-colcon build --symlink-install --packages-ignore rds_hud rds_sims rds_control rds_gui
+colcon build --symlink-install --packages-select rds_msgs
+source install/setup.bash
+colcon build --symlink-install --packages-ignore rds_sims
 source install/setup.bash
