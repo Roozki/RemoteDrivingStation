2ab3d2b7a20e6f608e1ed7b6490aabec46bcd517
Roozki <zawadzkirowan@gmail.com>
2024-04-11 00:21:00 -0700

    Quick Commit

--- src/rds_launchers/launch/on-board-bringup.launch.py
+++ src/rds_launchers/launch/on-board-bringup.launch.py
@@ -148,7 +148,8 @@ def generate_launch_description():
         ],
         parameters=[
             {'ffmpeg_image_transport.encoding': 'libx265',  # Use H.265 encoding with NVENC (if available)
-             'ffmpeg_image_transport.preset': 'medium',            # Low latency preset
+             'ffmpeg_image_transport.profile': 'high',
+             'ffmpeg_image_transport.preset': 'fast',            # Low latency preset
              'ffmpeg_image_transport.tune': 'zerolatency',         # Main profile for compatibility
              'ffmpeg_image_transport.gop_size': 15,            # GOP size (group of pictures)
             #  'ffmpeg_image_transport.bit_rate': 1000000,   # Target bitrate
@@ -167,7 +168,8 @@ def generate_launch_description():
         ],
         parameters=[
             {'ffmpeg_image_transport.encoding': 'libx265',  # Use H.265 encoding with NVENC (if available)
-             'ffmpeg_image_transport.preset': 'medium',            # Low latency preset
+             'ffmpeg_image_transport.profile': 'high',
+             'ffmpeg_image_transport.preset': 'fast',            # Low latency preset
              'ffmpeg_image_transport.tune': 'zerolatency',         # Main profile for compatibility
              'ffmpeg_image_transport.gop_size': 15,            # GOP size (group of pictures)
             #  'ffmpeg_image_transport.bit_rate': 1000000,   # Target bitrate
