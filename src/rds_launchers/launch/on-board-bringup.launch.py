306e43183286b4fb321bbf77c891550168c6d740
Roozki <zawadzkirowan@gmail.com>
2024-04-11 02:26:01 -0700

    Quick Commit

--- src/rds_launchers/launch/on-board-bringup.launch.py
+++ src/rds_launchers/launch/on-board-bringup.launch.py
@@ -151,13 +151,13 @@ def generate_launch_description():
              'ffmpeg_image_transport.profile': 'high422',
              'ffmpeg_image_transport.preset': 'ultrafast',            # Low latency preset
              'ffmpeg_image_transport.tune': 'zerolatency',         # Main profile for compatibility
-             'ffmpeg_image_transport.gop_size': 15,            # GOP size (group of pictures)
+             'ffmpeg_image_transport.gop_size': 10,            # GOP size (group of pictures)
              'ffmpeg_image_transport.bit_rate': 8242880,   # Target bitrate
-             'ffmpeg_image_transport.delay' : "0",
+             'ffmpeg_image_transport.delay' : "4",
              'ffmpeg_image_transport.frame_rate': 30,
              'ffmpeg_image_transport.qmax' : 180,
              'ffmpeg_image_transport.pixel_format': "yuv420p"
-            #  'ffmpeg_image_transport.crf': 28
+             'ffmpeg_image_transport.crf': 28
              }       
         ],
         arguments=['raw', 'in:=/vehicle_1/main_feed/image_raw', 'ffmpeg', 'out:=/vehicle_1/main_feed/image_raw/h265']
