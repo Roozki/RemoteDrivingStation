ef09228c783c5144a67e0026bd8837196866f499
Roozki <zawadzkirowan@gmail.com>
2024-04-11 02:57:21 -0700

    Quick Commit

--- src/rds_launchers/launch/on-board-bringup.launch.py
+++ src/rds_launchers/launch/on-board-bringup.launch.py
@@ -148,10 +148,10 @@ def generate_launch_description():
         ],
         parameters=[
             {'ffmpeg_image_transport.encoding': 'libx264',  # Use H.265 encoding with NVENC (if available)
-             'ffmpeg_image_transport.profile': 'main',
+             'ffmpeg_image_transport.profile': 'baseline',
              'ffmpeg_image_transport.preset': 'ultrafast',            # Low latency preset
              'ffmpeg_image_transport.tune': 'zerolatency',         # Main profile for compatibility
-             'ffmpeg_image_transport.gop_size': 15            # GOP size (group of pictures)
+             'ffmpeg_image_transport.gop_size': 5            # GOP size (group of pictures)
             #  'ffmpeg_image_transport.bit_rate': 8242880,   # Target bitrate
             #  'ffmpeg_image_transport.delay' : "0",
             #  'ffmpeg_image_transport.frame_rate': 25,
