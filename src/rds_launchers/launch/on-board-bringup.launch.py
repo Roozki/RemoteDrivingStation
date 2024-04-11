c9d2b1cd5515029a17cff91ea6fd2953472baed9
Roozki <zawadzkirowan@gmail.com>
2024-04-11 02:46:51 -0700

    Quick Commit

--- src/rds_launchers/launch/on-board-bringup.launch.py
+++ src/rds_launchers/launch/on-board-bringup.launch.py
@@ -152,7 +152,7 @@ def generate_launch_description():
              'ffmpeg_image_transport.preset': 'ultrafast',            # Low latency preset
              'ffmpeg_image_transport.tune': 'zerolatency',         # Main profile for compatibility
              'ffmpeg_image_transport.gop_size': 20,            # GOP size (group of pictures)
-             'ffmpeg_image_transport.bit_rate': 4242880,   # Target bitrate
+             'ffmpeg_image_transport.bit_rate': 2242880,   # Target bitrate
              'ffmpeg_image_transport.delay' : "2",
              'ffmpeg_image_transport.frame_rate': 25,
              'ffmpeg_image_transport.qmax' : 800,
