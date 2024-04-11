8094a8c95e6b85a064ae2cac4a41b79de95d2f1d
Roozki <zawadzkirowan@gmail.com>
2024-04-11 01:54:58 -0700

    Quick Commit

--- src/rds_launchers/launch/on-board-bringup.launch.py
+++ src/rds_launchers/launch/on-board-bringup.launch.py
@@ -153,7 +153,7 @@ def generate_launch_description():
              'ffmpeg_image_transport.tune': 'zerolatency',         # Main profile for compatibility
              'ffmpeg_image_transport.gop_size': 1,            # GOP size (group of pictures)
              'ffmpeg_image_transport.bit_rate': 8242880,   # Target bitrate
-             'ffmpeg_image_transport.delay' : "0",
+             'ffmpeg_image_transport.delay' : "4",
              'ffmpeg_image_transport.frame_rate': 25
             #  'ffmpeg_image_transport.crf': 28
              }       
@@ -176,7 +176,7 @@ def generate_launch_description():
              'ffmpeg_image_transport.gop_size': 5,            # GOP size (group of pictures)
              'ffmpeg_image_transport.bit_rate': 2242880,   # Target bitrate
              'ffmpeg_image_transport.frame_rate' : 15
-            #  'ffmpeg_image_transport.delay': "0.1"
+             'ffmpeg_image_transport.delay': "4"
             #  'ffmpeg_image_transport.crf': 28
              }       
         ],
