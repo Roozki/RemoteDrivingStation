7a19c3f8e16db708ff311d1d981cbd51fa99504c
Roozki <zawadzkirowan@gmail.com>
2024-04-11 12:58:29 -0700

    Quick Commit

--- xrandr_setup_g15.sh
+++ xrandr_setup_g15.sh
@@ -1,1 +1,1 @@
-xrandr --output HDMI-A-0 --mode 1920x1080 --right-of eDP
+xrandr --output eDP --primary --output DP-1-2 --mode 1920x1080 --left-of eDP  --output HDMI-A-0 --mode 1920x1080 --left-of DP-1-2
